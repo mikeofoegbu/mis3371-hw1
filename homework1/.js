1e20ca0705b732a6afc9344b27b197abfa93c653
mikeofoegbu <michaelcofoegbu2@gmail.com>
2026-02-28 22:03:29 -0600

    Update .js

--- homework1/.js
+++ homework1/.js
@@ -1,1 +1,18 @@
+/*
+Name: Michale Ofoegbu
+  Date created: 02/27/2026
+  Date last edited: 02/27/2026
+  Purpose: MIS 3371 Homework 1 HTML Form
+*/
 
+//dynamic date js code//
+const d = new Date();
+let text = d.toLocaleDateString();
+document.getElementById("today").innerHTML = text;
+
+//range slider js code//
+let slider = document.getElementById("range")
+  let output = document.getElementById("range-scale")
+  output.innerHTML = slider.value;
+
+slider.oninput = function () {output.innerHTML = this.value;};
