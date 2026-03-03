d6bd94df753a83c936ab62b82c48684dba7d8ab3
mikeofoegbu <michaelcofoegbu2@gmail.com>
2026-03-02 19:01:52 -0600

    Update script.js

--- homework1/script.js
+++ homework1/script.js
@@ -6,13 +6,13 @@ Name: Michale Ofoegbu
 */
 
 //dynamic date js code//
-const d = new Date();
-let text = d.toLocaleDateString();
-document.getElementById("today").innerHTML = text;
+const d = new Date(); // creates new Date object containing current system date
+let text = d.toLocaleDateString(); // converts date into readable local date format (MM/DD/YYYY)
+document.getElementById("today").innerHTML = text; // inserts formatted date into span element with id="today" in HTML header
 
 //range slider js code//
-let slider = document.getElementById("range")
-  let output = document.getElementById("range-scale")
-  output.innerHTML = slider.value;
+let slider = document.getElementById("range") // selects the range input element from the form
+  let output = document.getElementById("range-scale") // selects span element that displays current slider value
+  output.innerHTML = slider.value; // displays default slider value when page first loads
 
-slider.oninput = function () {output.innerHTML = this.value;};
+slider.oninput = function () {output.innerHTML = this.value;}; // updates displayed value dynamically whenever slider is moved
