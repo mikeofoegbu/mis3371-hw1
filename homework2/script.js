9b20036ab500252183911aeb30d174606ccf286e
mikeofoegbu <michaelcofoegbu2@gmail.com>
2026-03-20 16:13:11 -0500

    Update script.js

--- homework2/script.js
+++ homework2/script.js
@@ -1,1 +1,18 @@
+/*
+Name: Michael Ofoegbu
+  Date created: 02/22/2026
+  Date last edited: 03/02/2026
+  Purpose: MIS 3371 Homework 1 HTML Form
+*/
 
+//dynamic date js code//
+const d = new Date(); // creates new Date object containing current system date
+let text = d.toLocaleDateString(); // converts date into readable local date format (MM/DD/YYYY)
+document.getElementById("today").innerHTML = text; // inserts formatted date into span element with id="today" in HTML header
+
+//range slider js code//
+let slider = document.getElementById("range") // selects the range input element from the form
+  let output = document.getElementById("range-scale") // selects span element that displays current slider value
+  output.innerHTML = slider.value; // displays default slider value when page first loads
+
+slider.oninput = function () {output.innerHTML = this.value;}; // updates displayed value dynamically whenever slider is moved
