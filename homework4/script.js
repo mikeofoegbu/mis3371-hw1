fbf6af2d6ae1cd348e58128a127b6e18774cfb2e
mikeofoegbu <michaelcofoegbu2@gmail.com>
2026-04-25 15:02:26 -0500

    Update script.js

--- homework4/script.js
+++ homework4/script.js
@@ -458,3 +458,63 @@ function reviewInput() {
 function removeReview() {
     document.getElementById("showInput").innerHTML = "";
 }
+
+//cookie for remembering info input on form//
+function setCookie(name, cvalue, expiryDays) {
+    var day = new Date();
+    day.setTime(day.getTime() + (expiryDays*24*60*60*1000));
+    var expires = "expires=" + day.toUTCString();
+    document.cookie = name + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie() {
+    var cookieName = name + "=";
+    var cookies = document.cookie.split(';');
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i].trim();
+        while (cookie.charAt(0) == ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(cookieName) == 0) {
+            return cookie.substring(cookieName.length, cookie.length);
+        }
+    }
+    return "";
+}
+
+var inputs = [
+    {id: "fname", cookieName: "firstName"},
+    {id: "mini", cookieName: "middleInitial"},
+    {id: "lname", cookieName: "lastName"},
+    {id: "dob", cookieName: "dob"},
+    {id: "ssn", cookieName: "ssn"},
+    {id: "address1", cookieName: "address1"},
+    {id: "city", cookieName: "city"},
+    {id: "zcode", cookieName: "zipCode"},
+    {id: "email", cookieName: "email"},
+    {id: "phnum", cookieName: "phone"},
+    {id: "uname", cookieName: "userName"},
+];
+
+inputs.forEach(function(input) {
+    var inputElement = document.getElementById(input.id);
+
+    //prefill input fields with value from the cookie
+    var cookieValue = getCookie(input.cookieName);
+    if (cookieValue !== "") {
+        inputElement.value = cookieValue;
+    }
+
+    //set a cookie with the input value when the input field changes
+    inputElement.addEventListener("input", function() {
+        setCookie(input.cookieName, inputElement.value, 30);
+    });
+
+});
+
+//greet the user with their name + message if the cookie is set
+var firstName = getCookie("firstName");
+if (firstName !== "") {
+    document.getElementById("welcome").innerHTML = "Welcome back, " + firstName + "! </br>";
+}
